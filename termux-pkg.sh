57475de52d7762314d97dc71e0383e26fcfca9e3
tigerhackmalayalam <69100349+tigerhackmalayalam@users.noreply.github.com>
2020-09-01 16:43:58 +0530

    Update termux-pkg.sh

--- termux-pkg.sh
+++ termux-pkg.sh
@@ -1,13 +1,13 @@
 ﻿
 clear
-pip install lolcat -y
+
 pkg install toilet -y
 clear
 toilet -f mono12 -F border "TIGER"
 echo " "
-echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"
 echo " "
-echo "It may Take several minutes"| lolcat
+echo "It may Take several minutes"
 echo " "
 apt-get update -y
 apt-get upgrade -y
@@ -18,139 +18,139 @@ pkg install nano -y
 clear
 toilet -f mono12 -F border "TIGER"
 echo " "
-echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
-echo " "
-echo "It may Take several minutes"
-echo " "
-
-echo "installing perl"| lolcat
-echo " "
-pkg install perl -y
-clear
-toilet -f mono12 -F border "TIGER"
-echo " "
-echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
-echo " "
-echo "It may Take several minutes"
-echo " "
-
-echo "installing python"| lolcat
-echo " "
-pkg install python -y
-clear
-toilet -f mono12 -F border "TIGER"
-echo " "
-echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
-echo " "
-echo "It may Take several minutes"
-echo " "
-
-echo "installing python2"| lolcat
-echo " "
-pkg install python2 -y
-clear
-toilet -f mono12 -F border "TIGER"
-echo " "
-echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
-echo " "
-echo "It may Take several minutes"
-echo " "
-
-echo "installing figlet"| lolcat
-echo " "
-pkg install figlet -y
-clear
-toilet -f mono12 -F border "TIGER"
-echo " "
-echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
-echo " "
-echo "It may Take several minutes"
-echo " "
-
-echo "installing cmatrix"| lolcat
-echo " "
-pkg install cmatrix -y
-clear
-toilet -f mono12 -F border "TIGER"
-echo " "
-echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
-echo " "
-echo "It may Take several minutes"
-echo " "
-
-echo "installing pip"| lolcat
-echo " "
-pkg install pip -y
-clear
-toilet -f mono12 -F border "TIGER"
-echo " "
-echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
-echo " "
-echo "It may Take several minutes"
-echo " "
-
-echo "installing php"| lolcat
-echo " "
-pkg install php -y
-clear
-toilet -f mono12 -F border "TIGER"
-echo " "
-echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
-echo " "
-echo "It may Take several minutes"
-echo " "
-
-echo "installing ssh"| lolcat
-echo " "
-pkg install openssh -y
-clear
-toilet -f mono12 -F border "TIGER"
-echo " "
-echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
-echo " "
-echo "It may Take several minutes"
-echo " "
-
-echo "installing curl"| lolcat
-echo " "
-pkg install curl -y
-clear
-toilet -f mono12 -F border "TIGER"
-echo " "
-echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"
 echo " "
 echo "It may Take several minutes"
 echo " "
 
 echo "installing perl"
 echo " "
-pkg install perl -y| lolcat
+pkg install perl -y
 clear
 toilet -f mono12 -F border "TIGER"
 echo " "
-echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"
 echo " "
 echo "It may Take several minutes"
 echo " "
 
-echo "installing bash "| lolcat
+echo "installing python"
+echo " "
+pkg install python -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing python2"
+echo " "
+pkg install python2 -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing figlet"
+echo " "
+pkg install figlet -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing cmatrix"
+echo " "
+pkg install cmatrix -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing pip"
+echo " "
+pkg install pip -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing php"
+echo " "
+pkg install php -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing ssh"
+echo " "
+pkg install openssh -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing curl"
+echo " "
+pkg install curl -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing perl"
+echo " "
+pkg install perl -y 
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing bash "
 echo " "
 pkg install bash -y
 clear
 toilet -f mono12 -F border "TIGER"
 echo " "
-echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"
 echo " "
 echo "It may Take several minutes"
 echo " "
  
-echo "installing wget"| lolcat
+echo "installing wget"
 echo " "
 pkg install wget -y
 clear
 toilet -f mono12 -F border "TIGER"
 echo " "
-echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"
 echo " "
 echo "It may Take several minutes"
 echo " "
@@ -168,8 +168,8 @@ echo "YOU ARE ALL DONE "
 echo " "
 clear
 toilet -f mono12 -F border "TIGER"
-echo "FOR MORE DETAILS"| lolcat
-echo "SUBSCRIBE MY CHANNEL"| lolcat
+echo "FOR MORE DETAILS"
+echo "SUBSCRIBE MY CHANNEL"
 echo " CLICK  https://www.youtube.com/c/thmalayalam "
-echo "OR SEARCH {TIGER HACK MALAYALAM} ON YOUTUBE"| lolcat
+echo "OR SEARCH {TIGER HACK MALAYALAM} ON YOUTUBE"
 

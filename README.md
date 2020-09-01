587c068c5a47991223380f6df108e9e455d511eb
Ashik <thmalayalamchannel@gmail.com>
2020-09-01 15:11:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+This script was made by TIGER HACK MALAYALAM youtube channel
+For more search "Tiger Hack Malayalam"on YouTube
+Link :- https://www.youtube.com/c/thmalayalam
+To run this tool 
+
+First you want to install git 
+To install 
+pkg install git -y
+Then download the tool using git 
+git clone <link>
+
+
+
+To run the file termux-pkg.sh
+Type :- bash termux-pkg.sh
+
+In this tool I included all the basic packages for termux
+Packages. :- 
+* python
+*python2
+*toilet
+*figlet
+*lolcat
+*nano
+*perl
+*cmatrix
+*pip
+*php
+*wget
+*openssh
+*curl
+*perl
+*bash
+   etc.....
+By TIGER HACK MALAYALAM
---
+++ termux-pkg.sh
@@ -0,0 +1,175 @@
+﻿
+clear
+pip install lolcat -y
+pkg install toilet -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo " "
+echo "It may Take several minutes"| lolcat
+echo " "
+apt-get update -y
+apt-get upgrade -y
+echo "installing packages please wait"
+echo "installing nano"
+echo " "
+pkg install nano -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing perl"| lolcat
+echo " "
+pkg install perl -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing python"| lolcat
+echo " "
+pkg install python -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing python2"| lolcat
+echo " "
+pkg install python2 -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing figlet"| lolcat
+echo " "
+pkg install figlet -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing cmatrix"| lolcat
+echo " "
+pkg install cmatrix -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing pip"| lolcat
+echo " "
+pkg install pip -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing php"| lolcat
+echo " "
+pkg install php -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing ssh"| lolcat
+echo " "
+pkg install openssh -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing curl"| lolcat
+echo " "
+pkg install curl -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing perl"
+echo " "
+pkg install perl -y| lolcat
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo " "
+echo "It may Take several minutes"
+echo " "
+
+echo "installing bash "| lolcat
+echo " "
+pkg install bash -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo " "
+echo "It may Take several minutes"
+echo " "
+ 
+echo "installing wget"| lolcat
+echo " "
+pkg install wget -y
+clear
+toilet -f mono12 -F border "TIGER"
+echo " "
+echo "\. ..Script By {TIGER HACK MALAYALAM}.../"| lolcat
+echo " "
+echo "It may Take several minutes"
+echo " "
+echo " "
+clear
+toilet -f mono12 -F border TIGER 
+echo " "
+echo "YOUR ALL BASIC CODES ARE INSTALLED SUCCESSFULLY"
+echo " "
+echo "NOW YOU WANT TO ALLOW YOUR STORAGE PERMISSION"
+echo " "
+echo "PLEASE CLICK ALLOW"
+termux-setup-storage
+echo "YOU ARE ALL DONE "
+echo " "
+clear
+Figlet TIGER 
+echo "FOR MORE DETAILS"| lolcat
+echo "SUBSCRIBE MY CHANNEL"| lolcat
+echo " CLICK  https://www.youtube.com/c/thmalayalam "
+echo "OR SEARCH {TIGER HACK MALAYALAM} ON YOUTUBE"| lolcat
+
